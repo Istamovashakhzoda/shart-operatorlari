fe78f8f727dd421a90de538cf477d2fef3402a39
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-10 13:52:54 +0300

    Add files via upload

---
+++ practise/script.js
@@ -0,0 +1,320 @@
+"use script"
+//KIRITISH CHIQARISH OZLASHTIRISH OPERATSIYALARI
+
+
+//1-MASALA
+/*let a = Number(prompt("kvadrtini"));
+let P = 4*a;
+console.log(P);*/
+
+//2
+/*let a = Number(prompt("yuzini"));
+let S = a ** 2;
+console.log(S);*/
+
+//3
+/*let a =Number(prompt("kvadrt"));
+let b =Number(prompt("yuza"));
+let  S = a * b;
+alert (S);
+let P = 2 *( a+b );
+alert (P);*/
+
+
+//4
+/*let a = Number(prompt("diametr"));
+let b = Number(prompt("p-qiymat"));
+let L = p * d;
+alert (L); 
+let p =3.14;
+alert (p);*/
+
+//5-masala
+/*let a =Number(prompt("tomoni"));
+let b = Number(prompt("tomoni"));
+let V=a***3;
+console.log(V);
+let S =6 *a**2;
+console.log(S);*/
+
+//6-masala
+/*let a =Number(prompt("a-tomoni"));
+let b = Number(prompt("b-tomoni"));
+let c = Number(prompt("c-tomoni"));
+let V = a*b*c;
+console.log(V);
+let S = 2*(a*b+b*c+a*c);
+console.log(S);*/
+
+//7
+/*let s = Number(prompt("radius"));
+let l = Number(prompt("L"));
+let L = 2 * p * R;
+console.log(L);
+let S = p *R ** 2;
+console.log(S);*/
+
+//8
+/*let a = Number(prompt("tomoni"));
+let b =Number(prompt("tomoni"));
+let num = (a + b)/2;
+console.log(num);*/
+
+
+//9
+ /*let a = Number (prompt("a-son"));
+ let b = Number(prompt("b-son"));
+let kvd= (a * b) ** 1/2;
+console.log(kvd);*/
+
+//10
+/*let a = Number(prompt("a-son"));
+let b = Number(prompt("b-son"));
+console.log(nolga-tenmas);
+let = S a+b;
+let = B a*b;
+let = S (a+b)**2;
+console.log(yigindisi);*/
+
+//11-MASALA
+/*let a = Number (prompt("gipotenuza:"));
+let b = Number(prompt("premetr:"));
+let c =(a **2 + b**2)**1/2;
+console.log(c);
+let P =a+b+c;
+console.log(P);*/
+
+//12-MASALA
+/*let a = Number(prompt("radius1:"));
+let b = Number(prompt("radius2:"));
+let c = Number(prompt("radius3:"));
+let S1= p*R1;
+console.log(S1);
+let S2 = p*R2;
+console.log(S2);
+let S3=  p *R3;
+console.log(S3);*/
+
+//13-MASALA
+/*let a = Number(prompt("uzunligi:"));
+let b = Number(prompt("radius:"));
+let c = Number(prompt("yuzasi:"));
+let L = 2* p*R;
+console.log(L);
+let S = p*R**2;
+console.log(S);
+let p=3.14;
+console.log(p);*/
+
+//15-MASALA
+/*let a = Number(prompt("yuzasi:"));
+let b = Number(prompt("diametr:"));
+let c = Number(prompt("radius:"));
+let L = 2* p*R;
+console.log(L);
+let S= p*R **2;
+console.log(S);
+let p = 3.14;
+console.log(p);*/
+
+//16-MASALA
+/*let a= Number (prompt("masofasi:"));
+let x = x2-x1;
+console.log(x);*/
+
+//17-MASALA
+/*let a =Number(prompt("yig'indi"));
+let ac =(a-b);
+console.log(ac);
+let bc =(b-c);
+console.log(bc);
+let ab = ac+bc;
+console.log(ab);*/
+
+//17-MASALA
+
+
+//18-MASALA
+/*let a =Number(prompt("premetrini P, yuzasi S:"));
+let x1 = (x1);
+//console.log(x1);
+let y1 = (y1);
+//console.log(y1);
+let x2 = (x2);
+//console.log(x2);
+let y2 = (y2);
+//console.log(y2);
+let ab=(x1-x2);
+console.log(ab);
+let bc = (y1-y2);
+console.log(bc);
+let P = 2*(a+b);
+console.log(P);
+let S = a*b;
+console.log(S);*/
+
+//20- MASALA
+/*let a = Number(prompt("Masofasi topilsin:"));
+let e =(x1);
+//console.log(e);
+let b =(y1);
+//console.log(b);
+let c =(x2);
+//console.log(c);
+let d =(y2);
+//console.log(d);
+let masofa =((x1-x2)**2+(y1-y2)**2);
+console.log(masofa);*/
+
+//21-MASALA
+/*let a = Number(prompt("Premetre-P, Yuzasi-S topilsin:"));
+let b =(x1);
+//console.log(b);
+let c=(y1);
+//console.log(c);
+let d =(x2);
+//console.log(d);
+let e=(y2);
+//console.log(e);
+let f= (x3);
+//console.log(f);
+let i=(y3);
+//console.log(i);
+let ab =((x1-x2)**2 +(y1-y2)**2);
+console.log(ab);
+let bc =((x1-x3)**2 +(y1-y3)**2);
+console.log(bc);
+let cb =((x3-x2)**2 +(y3-y2)**2);
+console.log(cb);*/
+
+//22-MASALA
+/*let a = Number(prompt("A-ning qiymati:"));
+let b =Number(prompt("B-ning qiymati:"));
+let d=(a);
+alert(d);
+let c=(b);
+alert(c);
+*/
+
+//23-MASALA
+/*let a = Number(prompt("A-ning qiymati:"));
+let b =Number(prompt("B-ning qiymati:"));
+let c =Number(prompt("C-ning qiymati:"));
+let e=(b);
+alert(e);
+let i=(c);
+alert(i);
+let f=(a);
+alert(f)*/
+
+//24-MASALA
+/*let a = Number(prompt("A-ning qiymati:"));
+let b =Number(prompt("B-ning qiymati:"));
+let c =Number(prompt("C-ning qiymati:"));
+let e=(c);
+alert(e);
+let i=(a);
+alert(i);
+let f=(b);
+alert(f)*/
+
+//28-MASALA
+/*let a=Number(prompt("A-ning qiymatlari:"));
+let a2=(a**2);
+alert(a2);
+let a3=(a**3);
+alert(a3);
+let a5=(a**5);
+alert(a5);
+let a10=(a**10);
+alert(a10);
+let a15=(a**15);
+alert(a15);*/
+
+//25-MASALA
+/*let a=Number(prompt("X"));
+let y=(y=3*x**6-6*x**2-7);
+alert(y);*/
+
+//26-MASALA
+/*let a=Number(prompt("X"));
+let y=(y=4(x-3)**6-7(x-3)**3+2);
+alert(y);*/
+
+
+//27-MASALA
+/*let a=Number(prompt("A-2 darajasi:"));
+let b=Number(prompt("A-4 darajasi:"));
+let c=Number(prompt("A-8 darajasi:"));
+let a2=(a**2);
+alert(a2);
+let a4=(a**4);
+alert(a4);
+let a8=(a*8);
+alert(a8);*/
+
+
+//28-MASALA
+/*let a=Number(prompt("A-2 darajasi:"));
+let b=Number(prompt("A-3 darajasi:"));
+let c=Number(prompt("A-5 darajasi:"));
+let d=Number(prompt("A-10 darajasi:"));
+let e=Number(prompt("A-15 darajasi:"));
+let a2=(a**2);
+alert(a2);
+let a3=(a**3);
+alert(a3);
+let a5=(a**5);
+alert(a5);
+let a10=(a**10);
+alert(a10);
+let a15=(a**15);
+alert(a15);*/
+
+//29-MASALA
+/*let a=Number(prompt("A:"));
+let r=3.14;
+let gra=a*(r/180);
+alert(gra);*/
+
+//30-MASALA
+/*let a=Number(prompt("A:"));
+let p=3.14;
+let gra=a*(p/180);
+alert(gra);*/
+
+//31-MASALA
+/*let a=Number(prompt("TF"));
+let t=(tc=(tf-32)*5/9);
+alert(t);*/
+
+//32-masala
+/*let a=Number(prompt("TF"));
+let t=(tf=(tc*5/9)+32);
+alert(t);*/
+
+//33-MASALA
+let x=Number(prompt("X"));
+let y=Number(prompt("Y"));
+let b=(x/y);
+alert(b)
+let a=(y/1);
+alert(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
